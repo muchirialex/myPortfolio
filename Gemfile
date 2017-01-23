3588877b4dc9f995c31f87fd3160580cce6873c0
Alex Muchiri <muchiri94@gmail.com>
2017-01-23 18:28:28 +0300

    Setup contact form

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'redcarpet', '~> 3.4'
 gem 'will_paginate', '~> 3.1', '>= 3.1.5'
 gem 'friendly_id', '~> 5.2'
+gem 'mail_form', '~> 1.6'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/models/contact.rb
@@ -0,0 +1,14 @@
+class Contact < MailForm::Base
+	attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message,   :validate => true
+  attribute :nickname,  :captcha  => true
+
+  def header
+    {
+	  	:subject => "Contact Form",
+	  	:to => "muchiri94@gmail.com",
+	  	:from => %("#{name}" <#{email}>)
+    }
+  end
+end
